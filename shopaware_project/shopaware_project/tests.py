64ce61c54f2ee02a084f5cb1cdbc4c746727432f
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2016-01-28 16:08:06 -0800

    Add simple REST API scaffolding

---
+++ shopaware_project/shopaware_project/tests.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+
+class BaseTestCase(TestCase):
+
+    USERNAME = 'test'
+    PASSWORD = 'testtest'
+    EMAIL = 'rynliquid@gmail.com'
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+                        self.USERNAME,
+                        self.EMAIL,
+                        self.PASSWORD)
+
+    def tearDown(self):
+        self.user.delete()
+
+
+class ShopawareTestClient(Client)
+
+    def __init__(self, username=None, password=''):
+        self.username = username
+        self.password = password
+
+    def login(self, username=None, password=None):
+        if username:
+            self.user = User.objects.get(username=username)
+            self.password = password or self.password
+
+    def logout():
+        pass
